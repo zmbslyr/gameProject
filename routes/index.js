b18fa2d354a8d1a06efe0e8ba779cc21e310428f
Mark Hedgeland <mhedgeland@gmail.com>
2020-01-09 09:44:22 -0500

    [GLOBAL][js] Adds dynamic URL generation

--- routes/index.js
+++ routes/index.js
@@ -18,14 +18,19 @@ router.get('/about', (req, res, next) => {
   res.render('about', { title: 'About' });
 });
 
-// POST form
+// Dynamic URL generation for search result
 router.post('/submit-form', async (req, res, next) => {
+  res.redirect(`/${req.body.title}`);
   search = req.body.title;
   gameObj = await query(search);
+});
+
+// POST form
+router.get('/:term', (req, res, next) => {
   if (gameObj.name !== search) {
     res.render('gameNotFound', { title: 'Game Not Found', message: 'Game not found', desc: `"${search}" not found in library` });
   } else {
-    res.render('obj', { title: gameObj.name, name: gameObj.name, dev: gameObj.developer, rel: gameObj.releaseDate });
+    res.render('obj', { title: gameObj.name, name: gameObj.name, dev: gameObj.developer, rel: `Released: ${gameObj.releaseDate}` });
   }
 });
 
